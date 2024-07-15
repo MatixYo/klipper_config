461cf5f0ae2b309b25576f14d533e04de79dbf4c
MatixYo <matixovi@gmail.com>
2024-07-15 16:57:23 +0100

    add update mcu script

--- script/update_mcu.sh
+++ script/update_mcu.sh
@@ -3,39 +3,42 @@
 sudo service klipper stop
 cd ~/klipper
 
-# Update mcu rpi
-echo "Start update mcu rpi"
+# Update MCU RPi
+echo "Start update MCU RPi"
 echo ""
 make clean
+# Uncomment if menuconfig changes
 #make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
 make KCONFIG=/home/pi/klipper_config/script/config.linux_mcu
-read -p "mcu rpi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+read -p "MCU RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
 make flash KCONFIG_CONFIG=/home/pi/klipper_config/script/config.linux_mcu
-echo "Finish update mcu rpi"
+echo "Finish update MCU RPi"
 echo ""
 
-# Update mcu XYE
-echo "Start update mcu XYE"
+# Update MCU BTT EBB36
+echo "Start update BTT EBB36"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuXYE
-make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuXYE
-read -p "mcu XYE firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_lpc1769_1840010F871C4AAF863E7C5DC32000F5-if00 btt-skr-turbo-v1.4
-read -p "mcu XYE firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-echo "Finish update mcu XYE"
+# Uncomment if menuconfig changes
+#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.btt_ebb36
+make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.btt_ebb36
+read -p "MCU BTT EBB36 firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+python3 /home/pi/katapult/scripts/flash_can.py -f ./out/klipper.bin -u a5972f9dc61c
+read -p "MCU BTT EBB36 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+echo "Finish update MCU BTT EBB36"
 echo ""
 
-# Update mcu Z (!P1.29 setting)
-echo "Start update mcu Z"
+# Update MCU BTT Octopus
+echo "Start update BTT Octopus"
 echo ""
 make clean
-#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuZ
-make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.skr_turbo_14_mcuZ
-read -p "mcu Z firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-Klipper_lpc1769_07300110871C4AAFBF427C5DC72000F5-if00 btt-skr-turbo-v1.4
-read -p "mcu Z firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-echo "Finish update mcu Z"
+# Uncomment if menuconfig changes
+#make menuconfig KCONFIG_CONFIG=/home/pi/klipper_config/script/config.btt_octopus
+make KCONFIG_CONFIG=/home/pi/klipper_config/script/config.btt_octopus
+read -p "MCU BTT Octopus firmware built, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+python3 /home/pi/katapult/scripts/flash_can.py -f ./out/klipper.bin -d /dev/serial/by-id/usb-katapult_stm32f446xx_2F0040000650534E4E313020-if00
+read -p "MCU BTT Octopus firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+echo "Finish update MCU BTT Octopus"
 echo ""
 
 sudo service klipper start
